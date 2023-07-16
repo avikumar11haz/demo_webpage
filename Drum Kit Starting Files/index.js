d7650e9e5e1a4296cccb565f76b13ece3dcc3b1d
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-07-17 02:41:00 +0530

    create drum kit files and add function

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,7 @@
+var numberofDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberofDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    alert("I got clicked");
+  });
+}
