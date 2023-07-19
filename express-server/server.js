791d93cf3539ce59dbc800dea39a7120624cdb82
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-07-20 03:21:54 +0530

    learn node and express js -- making routes

---
+++ express-server/server.js
@@ -0,0 +1,21 @@
+//jshint esversion:6
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (request, response) {
+  response.send("hello");
+});
+
+app.get("/contact", function (req, res) {
+  res.send("Hope you are well and good listener");
+});
+
+app.get("/hobbies", function (req, res) {
+  res.send("palying cricket, and dancing");
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
